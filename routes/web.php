eb0bc5abd637feb0611daf2bf721752cfbec0672
Peng Tao <pengtao@hudunsoft.com>
2021-01-19 16:57:53 +0800

    收获地址列表

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
-//Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+});
