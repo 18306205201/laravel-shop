9471fbad2d4d9669a5d8554c31335ce018010315
Peng Tao <pengtao@hudunsoft.com>
2021-01-22 16:10:10 +0800

    用户界面-商品列表搜索、排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,7 +9,36 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
-        return view('products.index', compact('products'));
+        $builder = Product::query()->where('on_sale', true);
+        // 判断是否有 search 参数
+        if ($search = $request->input('search', '')) {
+            $like = '%' . $search . '%';
+            // 模糊搜索商品标题，商品描述，SKU标题，SKU 描述
+            $builder->where(function ($query) use ($like) {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like) {
+                        $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
+        // 是否有提交 order 参数
+        if ($order = $request->input('order', '')) {
+            // 是否以 _asc 或者 _desc结尾
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        $products = $builder->paginate(16);
+        return view('products.index', [
+            'products' => $products,
+            'filters'  => [
+                'search' => $search,
+                'order'  => $order
+            ]
+        ]);
     }
 }
