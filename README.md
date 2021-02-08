f6f48f991cc85b2b5195caa1231f3dcfb7ea226e
vagrant <vagrant@homestead>
2021-02-08 01:38:53 +0000

    引入支付库

--- README.md
+++ README.md
@@ -12,5 +12,17 @@ DB_DATABASE=laravel
 DB_USERNAME=root
 
 DB_PASSWORD=
-
+## 执行 composer install
 composer install
+## 生成应用 key
+php artisan key:generate
+## 执行数据迁移，构建表结构
+php artisan migrate
+## 生成测试数据
+php artisan db:seed
+## 配置前端样式
+yarn config set registry https://registry.npm.taobao.org
+
+SASS_BINARY_SITE=http://npm.taobao.org/mirrors/node-sass yarn
+
+npm run watch-poll
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 往服务容器中注入 alipay 对象
+        $this->app->singleton('alipay', function() {
+            $config = config('pay.alipay');
+            // 判断当前是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 使用 yansongda\pay 创建支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function() {
+            $config = config('pay.wechat');
+            // 判断当前是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 使用 yansongda\pay 创建微信支付对象
+            return Pay::wechat($config);
+        });
     }
 
     /**
