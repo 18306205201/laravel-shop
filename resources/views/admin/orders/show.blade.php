2b77366dae651cdbbf468132f1de10f5250b21f4
vagrant <vagrant@homestead>
2021-02-19 15:58:11 +0000

    bug

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -1,10 +1,9 @@
-
 <div class="box box-info">
     <div class="box-header with-border">
         <h3 class="box-title">订单流水号：{{ $order->no }}</h3>
         <div class="box-tools">
             <div class="btn-group float-right" style="margin-right: 10px">
-                <a href="{{ route('admin.orders.index') }}" class="btn btn-sm btn-default"><i class="fa fa-list"></i> 列表</a>
+{{--                <a href="{{ route('admin.orders.index') }}" class="btn btn-sm btn-default"><i class="fa fa-list"></i> 列表</a>--}}
             </div>
         </div>
     </div>
@@ -15,7 +14,7 @@
                 <td>买家：</td>
                 <td>{{ $order->user->name }}</td>
                 <td>支付时间：</td>
-                <td>{{ $order->paid_at->format('Y-m-d H:i:s') }}</td>
+                <td>{{ $order->paid_at }}</td>
             </tr>
             <tr>
                 <td>支付方式：</td>
@@ -28,7 +27,7 @@
                 <td colspan="3">{{ $order->address['address'] }} {{ $order->address['zip'] }} {{ $order->address['contact_name'] }} {{ $order->address['contact_phone'] }}</td>
             </tr>
             <tr>
-                <td rowspan="{{ $order->items->count() + 1 }}">商品列表</td>
+                <td rowspan="{{ $order->orderItem->count() + 1 }}">商品列表</td>
                 <td>商品名称</td>
                 <td>单价</td>
                 <td>数量</td>
